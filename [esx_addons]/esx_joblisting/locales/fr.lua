ba4d08cf141ff7c2c8005837195d720ce8891877
WeeskyBDW <bdwadv@gmail.com>
2021-07-18 06:18:57 +0200

    :globe_with_meridians: Change french message new_job

--- [esx_addons]/esx_joblisting/locales/fr.lua
+++ [esx_addons]/esx_joblisting/locales/fr.lua
@@ -1,5 +1,5 @@
 Locales['fr'] = {
-  ['new_job'] = 'vous avez un nouveau job !',
+  ['new_job'] = 'Votre point GPS a été défini sur le vestiaire de votre métier',
   ['access_job_center'] = 'appuyez sur ~INPUT_PICKUP~ pour \naccéder au ~b~Pôle Emploi~s~.',
   ['job_center'] = 'pôle-Emploi',
 }
